693c8b8667559a060673bac56c124fa9edd8c029
Vlad <vlad.melnik@bk.ru>
2022-07-25 00:31:57 +0300

    task 1 added

---
+++ TASK-1/Program.cs
@@ -0,0 +1,12 @@
+﻿int Digitsum(int N)
+{
+    if(N > 9) 
+    return 1 + Digitsum(N = N/10);
+    else return 1;
+    
+
+}
+Console.WriteLine("Input N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine(Digitsum(n));
