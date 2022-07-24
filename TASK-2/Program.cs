4ba2a19635c3dea19320913ec6c0eaa9f50c4bf4
Vlad <vlad.melnik@bk.ru>
2022-07-25 00:34:24 +0300

    2 task added

---
+++ TASK-2/Program.cs
@@ -0,0 +1,14 @@
+﻿int Sumnatur(int M, int N)
+{
+    if(M < 0) M = 1;
+    if(M < N) return M + Sumnatur(M + 1, N);
+    else return N;
+    
+
+}
+Console.WriteLine("Input M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine(Sumnatur(m,n));
